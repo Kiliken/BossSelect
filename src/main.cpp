215b74efd0353bfe485b1f7da5690ec18e4750b4
Zayar Aung <z.zayar99@gmail.com>
2026-08-27 18:14:10 +0900

    background image test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
-#include "bn_core.h"
+#include "bn_core.h"                        // Butano core functions
+#include "bn_regular_bg_ptr.h"              // regular background pointers
+#include "bn_regular_bg_items_testbg.h"     // the test background item
 
 int main()
 {
+    // Initialize the Butano core system
     bn::core::init();
+
+    // Instantiate the background at the X, Y coordinates (0, 0)
+    bn::regular_bg_ptr bg = bn::regular_bg_items::testbg.create_bg(0, 0);
 
     while(true)
     {
